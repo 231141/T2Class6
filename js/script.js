f0a9833b11e503f0797d4b0d221670c8033cca8b
231141 <231141@virtualwindow.co.za>
2024-05-28 12:57:21 +0200

    done

---
+++ js/script.js
@@ -0,0 +1,76 @@
+let numbers = [40, 100, 1];
+let points = new Array (40, 100, 1);
+
+console.log(numbers);
+console.log(points);
+
+let good = [40];
+let bad = new Array (40);
+
+console.log(good);
+console.log(bad);
+
+let dogs = ["golden retriever", "labrador", "staffie", "bulldog", "rotweiller", "toi-pomme"]
+
+console.log(dogs);//whole array
+console.log(dogs.length);//just length
+console.log(dogs[0]);//specific item
+console.log(dogs[dogs.length-1]);//last item
+console.log(Array.isArray(dogs));//is array?
+console.log(dogs.toString());
+console.log(dogs.join(", "));
+
+dogs.pop();
+console.log(dogs);
+
+dogs.push("toi-pomme");
+console.log(dogs);
+
+let dog = dogs.pop();
+
+console.log(dog);
+console.log(dogs);
+
+let newDogs = dogs.slice(1, 4);
+
+console.log(newDogs);
+console.log(dogs);
+
+dogs.splice(1, 2, "jack russel", "doberman", "pitbull");
+
+console.log(dogs);
+
+function decreaseMars(){
+    if (document.getElementById('ticketsMars').value != 0){
+        document.getElementById('ticketsMars').value = parseInt(document.getElementById('ticketsMars').value) - 1;
+    } else{
+        document.getElementById('ticketsMars').value = 0;
+    }
+}
+
+function increaseMars(){
+    document.getElementById('ticketsMars').value = parseInt(document.getElementById('ticketsMars').value) + 1;
+}
+
+function decreaseSaturn(){
+    if (document.getElementById('ticketsSaturn').value != 0){
+        document.getElementById('ticketsSaturn').value = parseInt(document.getElementById('ticketsSaturn').value) - 1;
+    } else{
+        document.getElementById('ticketsSaturn').value = 0;
+    }
+}
+
+function increaseSaturn(){
+    document.getElementById('ticketsSaturn').value = parseInt(document.getElementById('ticketsSaturn').value) + 1;
+}
+
+let totalMarsCost = 0;
+
+function total(){
+    totalMarsCost = parseInt(document.getElementById('ticketsMars').value) * 50;
+    document.getElementById('total').innerHTML = "R " + totalMarsCost;
+}
+
+
+
+console.log();
